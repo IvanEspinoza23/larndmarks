e34877dc5a0eb1703f8b08c04f3e35d16b39a4af
IvanEspinoza23 <102266339+IvanEspinoza23@users.noreply.github.com>
2022-03-31 08:31:58 -0700

    Add files via upload

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,44 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Ivan on 3/23/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelDate: ModelData
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+         modelDate.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
